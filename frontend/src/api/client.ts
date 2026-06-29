0f443b774e5d672d0080d361e026c9c85d0256ab
Krishal-D <22151706@student.westernsydney.edu.au>
2026-06-30 02:02:42 +1000

    Use deployed API URL

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,10 +1,9 @@
 import axios from 'axios'
 
 const client = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: `${import.meta.env.VITE_API_URL}/api`,
     withCredentials: true,
 })
-
 client.interceptors.request.use((config) => {
     const token = localStorage.getItem('token')
 
